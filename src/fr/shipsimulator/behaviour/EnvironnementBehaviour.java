84ef471ff7d9b50bc50c220ef2c6f10e26742958
Villain Benoit <villain.benoit.dev@gmail.com>
2015-05-30 19:39:59 +0200

    gui working =3 .... quite well =D

--- src/fr/shipsimulator/behaviour/EnvironnementBehaviour.java
+++ src/fr/shipsimulator/behaviour/EnvironnementBehaviour.java
@@ -1,12 +1,14 @@
 package fr.shipsimulator.behaviour;
 
+import fr.shipsimulator.agent.EnvironnementAgent;
 import jade.core.behaviours.CyclicBehaviour;
 
 public class EnvironnementBehaviour extends CyclicBehaviour {
 
 	@Override
 	public void action() {
-
+		EnvironnementAgent ea = (EnvironnementAgent) this.myAgent;
+		//ea.getMyGui().refreshDisplay();
 	}
 
 }
--- src/fr/shipsimulator/constantes/Constante.java
+++ src/fr/shipsimulator/constantes/Constante.java
@@ -8,5 +8,9 @@ public interface Constante {
 	//MAP
 	int MAP_H = 600;
 	int MAP_W = 800;
+	int DEFAULT_COLS = 30;
+	int DEFAULT_ROWS = 30;
 	String MAP_PATH = "./ressource/carte.png";
+	String BATEAU_PATH = "./ressource/bateau_icone.png";
+
 }
