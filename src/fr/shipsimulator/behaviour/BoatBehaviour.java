e971024361281b6b30d75cee9ffbc50909550f20
Guillaume Vassal <gp.vassal@gmail.com>
2015-06-04 13:54:44 +0200

    Ajout block() pour ne pas faire grimper le cpu gratuitement pendant le dev

--- src/fr/shipsimulator/behaviour/BoatBehaviour.java
+++ src/fr/shipsimulator/behaviour/BoatBehaviour.java
@@ -1,37 +1,37 @@
 package fr.shipsimulator.behaviour;
 
-import fr.shipsimulator.structure.Boat;
-import fr.shipsimulator.structure.Player;
 import jade.core.behaviours.CyclicBehaviour;
 
 public class BoatBehaviour extends CyclicBehaviour {
 	
 	public BoatBehaviour() {
-		// Creer des agents membre d'équipage et les ajouter au bateau
+		// Creer des agents membre d'ï¿½quipage et les ajouter au bateau
 	}
 	
 	public void action() {
-		// 1. Attendre des ordres de l'équipe sur demande
-			// 1.1 Attendre un ordre de combat -> simuler attaque, communiquer à env le bateau concerné, nombre impacts et dégats
-			// 1.2 Attendre un ordre de mouvement -> changer X/Y (information env auto après)
+		// 1. Attendre des ordres de l'ï¿½quipe sur demande
+			// 1.1 Attendre un ordre de combat -> simuler attaque, communiquer ï¿½ env le bateau concernï¿½, nombre impacts et dï¿½gats
+			// 1.2 Attendre un ordre de mouvement -> changer X/Y (information env auto aprï¿½s)
 			// Request, from crewMember, json Order, type = combat ou mouvement
 		
-		// 4. Engager un combat sur demande d'environnement (subir des dégats)
+		// 4. Engager un combat sur demande d'environnement (subir des dï¿½gats)
 					// -> recevoir d'env le nombre d'impacts et les degats
 		
-		// 2. Informer un membre d'équipage de la composition de l'équipage sur demande
-			// Il faudrait synchroniser ça avec un DFAgentDescription à voir, voir ne garder que ça
-			// Eventuellement aussi avoir un behaviour rien que pour ça, parce qu'en fait ça pourrait rester cyclic ça !
+		// 2. Informer un membre d'ï¿½quipage de la composition de l'ï¿½quipage sur demande
+			// Il faudrait synchroniser ï¿½a avec un DFAgentDescription ï¿½ voir, voir ne garder que ï¿½a
+			// Eventuellement aussi avoir un behaviour rien que pour ï¿½a, parce qu'en fait ï¿½a pourrait rester cyclic ï¿½a !
 		
-		// 3. Informer l'agent environnement de sa position, à chaque fois
+		// 3. Informer l'agent environnement de sa position, ï¿½ chaque fois
 			// -> inform vers env
 		
 		// 5. Echanger avec une ville (+ agent mission ?), sur demande
-			// A voir ça quand ville et mission seront écrits
+			// A voir ï¿½a quand ville et mission seront ï¿½crits
 		
 		// 6. Informer l'agent environnement en cas de destruction
 			// verifier si boat.isDestroyed() et inform env
 		
+		// Pour ne pas faire monter le cpu en dev
+		block();
 	}
 
 }
