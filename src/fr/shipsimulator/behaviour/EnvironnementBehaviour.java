5d8b0265b761e55ef2628312c6a69a154904732a
Ines K <ine.kapp@gmail.com>
2015-06-06 13:17:53 +0200

    envoi reponse serialisee aux observer

--- src/fr/shipsimulator/behaviour/EnvironnementBehaviour.java
+++ src/fr/shipsimulator/behaviour/EnvironnementBehaviour.java
@@ -3,6 +3,13 @@ package fr.shipsimulator.behaviour;
 import jade.core.AID;
 import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import fr.shipsimulator.agent.EnvironnementAgent;
 import fr.shipsimulator.constantes.Constante;
 
@@ -29,24 +36,46 @@ public class EnvironnementBehaviour extends Behaviour {
 					int[][] vision = new int[2 * portee + 1][2 * portee + 1];
 					for(int i = 0; i <= portee ; i++){
 						for(int j = 0; j <= portee ; j++){
-							if(posX + i < ea.getMainGui().getCols()){
-								vision[portee + i][portee + j] = 
-										posY + j < ea.getMainGui().getRows() ? ea.getMapData()[posX + i][posY + j] : Constante.LAND;
-								vision[portee + i][portee - j] = 
-										posY - j >= 0 ? ea.getMapData()[posX + i][posY - j] : Constante.LAND;
+							if(i == 0 && j == 0){
+								vision[i][j] = Constante.SELF;
 							}
-							if(posX - i >= 0){
-								vision[portee - i][portee + j] = 
-										posY + j < ea.getMainGui().getRows() ? ea.getMapData()[posX - i][posY + j] : Constante.LAND;
-								vision[portee - i][portee - j] = 
-										posY - j >= 0 ? ea.getMapData()[posX - i][posY - j] : Constante.LAND;
+							else {
+								if(posX + i < ea.getMainGui().getCols()){
+									vision[portee + i][portee + j] = 
+											posY + j < ea.getMainGui().getRows() ? ea.getMapData()[posX + i][posY + j] : Constante.LAND;
+									vision[portee + i][portee - j] = 
+											posY - j >= 0 ? ea.getMapData()[posX + i][posY - j] : Constante.LAND;
+								}
+								if(posX - i >= 0){
+									vision[portee - i][portee + j] = 
+											posY + j < ea.getMainGui().getRows() ? ea.getMapData()[posX - i][posY + j] : Constante.LAND;
+									vision[portee - i][portee - j] = 
+											posY - j >= 0 ? ea.getMapData()[posX - i][posY - j] : Constante.LAND;
+								}
 							}
 						}
 					}
-					vision[0][0] = Constante.SELF;
-					//TODO envoi reponse serialisée
-					//ACLMessage reply = msg.createReply();
-					//reply.setContent(content); //tableau sérialisé
+					
+					List<List<Integer>> visionList = new ArrayList<List<Integer>>();
+					//chaque liste est une ligne du tableau et contient donc 2 * portee + 1 valeurs
+					for(int i = 0; i < 2 * portee + 1; i++){
+						visionList.add(new ArrayList<Integer>());
+						for(int j = 0; j < 2 * portee + 1; j++){
+							visionList.get(i).add(vision[j][i]);
+						}
+					}
+					
+					ObjectMapper mapper = new ObjectMapper();
+					String serialized = "";
+					try {
+						serialized = mapper.writeValueAsString(visionList);
+					} catch (JsonProcessingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					ACLMessage reply = msg.createReply();
+					reply.setContent(serialized);
+					ea.send(reply);
 				}
 			}
 			//else if matches pour les autres agents...
