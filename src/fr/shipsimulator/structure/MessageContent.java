298bfa4e9621883806aea23b8e2ee74128f64957
Ines K <ine.kapp@gmail.com>
2015-06-07 10:59:30 +0200

    ajout désérialisation dans classe MessageContent

--- src/fr/shipsimulator/structure/MessageContent.java
+++ src/fr/shipsimulator/structure/MessageContent.java
@@ -1,9 +1,11 @@
 package fr.shipsimulator.structure;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MessageContent {
@@ -20,6 +22,19 @@ public class MessageContent {
 			e.printStackTrace();
 		}
 		return serialized;
+	}
+	
+	public static List<Integer> deserialize(String serialized){
+		ObjectMapper mapper = new ObjectMapper();
+		List<Integer> deserialized = new ArrayList<Integer>();
+		try {
+			deserialized = mapper.readValue(serialized, new TypeReference<List<Integer>>(){});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return deserialized;
 	}
 	
 }
