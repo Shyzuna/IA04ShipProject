eceb10454b3ed13a999d5d5aeb59e9c50679b1f4
Guillaume Vassal <gp.vassal@gmail.com>
2015-06-10 12:18:33 +0200

    Transfert fixe

---
+++ src/fr/shipsimulator/structure/Mission.java
@@ -0,0 +1,10 @@
+package fr.shipsimulator.structure;
+
+public class Mission {
+	private Ville departure;
+	private Ville arrival;
+	
+	private Resource ressource;
+	private Integer resourceAmount;
+	
+}
