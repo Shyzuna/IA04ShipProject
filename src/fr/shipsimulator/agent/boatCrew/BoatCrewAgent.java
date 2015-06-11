a75d4fc58007acd531a680652d455caefc5c2511
Guillaume Vassal <gp.vassal@gmail.com>
2015-06-11 15:30:05 +0200

    Debut behaviour captain

--- src/fr/shipsimulator/agent/boatCrew/BoatCrewAgent.java
+++ src/fr/shipsimulator/agent/boatCrew/BoatCrewAgent.java
@@ -1,8 +1,17 @@
 package fr.shipsimulator.agent.boatCrew;
 
 import jade.core.Agent;
+import fr.shipsimulator.behaviour.ObserverObservBehaviour;
+import fr.shipsimulator.behaviour.ObserverVoteBehaviour;
+import fr.shipsimulator.gui.MainGui;
 
 public class BoatCrewAgent extends Agent {
 	private static final long serialVersionUID = 1L;
+	
+	public void setup(){
+		this.addBehaviour(new ObserverObservBehaviour());
+		this.addBehaviour(new ObserverVoteBehaviour());
+		MainGui.writeLog("BoatCrewAgent", this.getLocalName() + " sur le pont");
+	}
 	
 }
--- src/fr/shipsimulator/behaviour/CaptainMissionBehaviour.java
+++ src/fr/shipsimulator/behaviour/CaptainMissionBehaviour.java
@@ -1,18 +1,65 @@
 package fr.shipsimulator.behaviour;
 
+import jade.core.AID;
 import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import fr.shipsimulator.gui.MainGui;
+import fr.shipsimulator.structure.City;
+import fr.shipsimulator.structure.GenericMessageContent;
+import fr.shipsimulator.structure.Mission;
 
 public class CaptainMissionBehaviour extends Behaviour{
 	private static final long serialVersionUID = 1L;
-
+	
+	private Mission	mission;
+	
+	public CaptainMissionBehaviour() {
+		MainGui.writeLog("CaptainMissionBehaviour", "Demande des missions disponibles");
+		askAvailableMission(new City(0, 0));
+		
+		MainGui.writeLog("CaptainMissionBehaviour", "Vote pour choisir une mission");
+	}
+	
 	@Override
 	public void action() {
-		block();
+		MessageTemplate mt = new MessageTemplate(new MissionResponse());
+		ACLMessage request = myAgent.receive(mt);
+		if (request != null) {
+			
+		}
+		else block();
 		
 	}
 
 	@Override
 	public boolean done() {
 		return false;
-	}		
+	}	
+	
+	private void askAvailableMission(City city){		
+		ACLMessage envRequest = new ACLMessage(ACLMessage.REQUEST);
+		envRequest.addReceiver(new AID("Mission", AID.ISLOCALNAME));
+		myAgent.send(envRequest);
+	}
+	
+	private void submitVoteToCrew(GenericMessageContent<String> missions){		
+		ACLMessage envRequest = new ACLMessage(ACLMessage.REQUEST);
+		envRequest.addReceiver(new AID("Mission", AID.ISLOCALNAME));
+		myAgent.send(envRequest);
+	}
+	
+	private class MissionRequest implements MessageTemplate.MatchExpression {
+		private static final long serialVersionUID = 1L;
+		public boolean match(ACLMessage msg) {
+	    	return msg.getContent().matches("MissionRequest$:!(.*)") && (msg.getPerformative() == ACLMessage.REQUEST);
+	    }
+	}
+	
+	private class MissionResponse implements MessageTemplate.MatchExpression {
+		private static final long serialVersionUID = 1L;
+		public boolean match(ACLMessage msg) {
+	    	return msg.getContent().matches("MissionResponse(.*)") && msg.getPerformative() == ACLMessage.INFORM;
+	    }
+	}
 }
